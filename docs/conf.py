8ded670a5f71d656acb588bdfd38c6c281be48ef
Erik-Toller_wsp <erik.toller@wsp.com>
2025-02-17 10:53:29 +0100

    documentation

--- docs/conf.py
+++ docs/conf.py
@@ -33,5 +33,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "bizstyle"
 html_static_path = ['_static']
+html_theme_options = {
+    "navbar_align": "content",
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/eriktoller/andfn",  # required
+            "icon": "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg",
+            "type": "url",
+        }],
+}
